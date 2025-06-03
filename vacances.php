92836c61e08d7fec5659dab288d45277fbaa5848
simo04 <simoooxelasli@gmail.com>
2025-06-03 22:06:59 +0100

    add a folder for css files and clear some codes

--- vacances.php
+++ vacances.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OFPPT Website</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="vacances.css">
+    <link rel="stylesheet" href="css/vacances.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" type="x-icon" href="images&videos/ofppt.png"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
