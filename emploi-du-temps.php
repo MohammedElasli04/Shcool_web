4a2ee7138063a953a44920d46c27a3ec75bb7044
simo04 <simoooxelasli@gmail.com>
2025-06-05 21:00:25 +0100

    clearing some codes in emploie page and add it in css folder

--- emploi-du-temps.php
+++ emploi-du-temps.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="emploi.css">
+    <link rel="stylesheet" href="css/emploi.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" type="x-icon" href="images&videos/ofppt.png"> 
@@ -12,9 +12,7 @@
 </head>
 <body>
     <?php
-       
-       include("header.html");
-
+        include("header.html");
     ?>
 <!-- background image et lignes -->
 
