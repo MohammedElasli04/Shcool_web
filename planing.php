f4a8c67b6f45c05132c353538f3e42087ee6e2ac
simo04 <simoooxelasli@gmail.com>
2025-06-07 20:00:35 +0100

    simplify some codes in planning page to

--- planing.php
+++ planing.php
@@ -9,7 +9,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
    <link rel="shortcut icon" type="x-icon" href="images&videos/ofppt.png"> 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
-   <link rel="stylesheet" href="planing.css">
+   <link rel="stylesheet" href="css/planing.css">
   <style>
     /* General container styling */
 .calendar-container {
