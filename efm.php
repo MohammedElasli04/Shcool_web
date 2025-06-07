9485e74c26e3f7969aa3bff6b5fa2d614deafc85
simo04 <simoooxelasli@gmail.com>
2025-06-07 19:56:24 +0100

    simplify some codes in efm page

--- efm.php
+++ efm.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OFPPT Website</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="efm.css">
+    <link rel="stylesheet" href="css/efm.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="shortcut icon" type="x-icon" href="images&videos/ofppt.png"> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
@@ -16,10 +16,8 @@
 
 <body>
     <?php
-       
-          include("header.html");
-
-     ?>
+        include("header.html");
+    ?>
 <!-- background image et lignes -->
 
     <div class="animated-section">
@@ -53,7 +51,7 @@
 
 
 
-   <!-- Dropdown container -->
+<!-- Dropdown container -->
 <div class="dropdown-container">
     <label for="group" class="select">Select Group:</label>
     <select class="groupefm" id="group">
@@ -116,11 +114,9 @@ window.addEventListener("load", () => {
     </script>
 
 <!-- footer -->
-   <?php 
-       
-       include("footer.html") ;
-  
-   ?>
+    <?php 
+        include("footer.html") ;
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
 </body>
