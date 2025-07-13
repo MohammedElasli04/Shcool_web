4104af155ea82ba63a4b749aa61978a91b56bcea
simo04 <simoooxelasli@gmail.com>
2025-07-13 23:15:49 +0100

    clear codes in formation page

--- Formation-dip.php
+++ Formation-dip.php
@@ -13,9 +13,7 @@
 </head>
 <body>
     <?php
-       
         include("header.html");
-
     ?>
     <div class="slider">
         <figure>
@@ -31,64 +29,58 @@
             <div class="slide">
                 <img src="images&videos/f8392592-8bfd-4eb6-aa67-19e10800fa9f.jpg" alt="" class="dark">
                 <div id="typing-container">
-                 <strong>   Let's make a better future <br>
-                     <span class="blue">together</span></strong>
-                  </div>
+                    <strong>   Let's make a better future <br>
+                        <span class="blue">together</span>
+                    </strong>
+                </div>
             </div>
         </figure>
     </div>
-
     <section id="formation">
         <H1 id="titre">Les filières assurées à cet <span>établissement</span></H1>
-    <div class="formation">
-      <div class="filier card" style="width: 21rem;">
-        <img class="card-img-top" src="images&videos/dd.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Développement Digital</h5>
-          <p class="card-text">Le tronc commun en Développement Digital est une
-             étape importante pour acquérir les bases nécessaires à l'étude
-            , la conception, la construction, le...</p>
-          <a href="https://www.myway.ac.ma/fr/filiere/DIA_DEV_TS" class="btn btn-primary" target="_blank">More Details</a>
+        <div class="formation">
+            <div class="filier card" style="width: 21rem;">
+                <img class="card-img-top" src="images&videos/dd.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Développement Digital</h5>
+                    <p class="card-text">Le tronc commun en Développement Digital est une
+                      étape importante pour acquérir les bases nécessaires à l'étude
+                      , la conception, la construction, le...</p>
+                    <a href="https://www.myway.ac.ma/fr/filiere/DIA_DEV_TS" class="btn btn-primary" target="_blank">More Details</a>
+                </div>
+            </div>
+            <div class="filier card" style="width: 21rem;">
+                <img class="card-img-top" src="images&videos/Gestion-Entreprise.jpeg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Gestion des Entreprises</h5>
+                    <p class="card-text">Le tronc commun Gestion des Entreprises donne au stagiaire toutes les
+                      compétences nécessaires pour découvrir le monde des métiers de gestion et du commerce,...</p>
+                    <a href="https://www.myway.ac.ma/fr/filiere/GC_GE_TS" class="btn btn-primary" target="_blank">More Details></a>
+                </div>
+            </div>
+            <div class="filier card" style="width: 21rem;">
+                <img class="card-img-top" src="images&videos/assistant-admin-comm_thomas_31052023.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Assistant Administratif</h5>
+                    <p class="card-text">Le tronc commun de la filière Assistant Administratif donne au stagiaire les compétences
+                      nécessaires pour découvrir les métiers de gestion et du commerce,...</p>
+                    <a href="https://www.myway.ac.ma/fr/filiere/GC_AA_T" class="btn btn-primary" target="_blank">More details</a>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div class="filier card" style="width: 21rem;">
-        <img class="card-img-top" src="images&videos/Gestion-Entreprise.jpeg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Gestion des Entreprises</h5>
-          <p class="card-text">Le tronc commun Gestion des Entreprises donne au stagiaire toutes les
-             compétences nécessaires pour découvrir le monde des métiers de gestion et du commerce,...</p>
-          <a href="https://www.myway.ac.ma/fr/filiere/GC_GE_TS" class="btn btn-primary" target="_blank">More Details></a>
-        </div>
-      </div>
-
-      <div class="filier card" style="width: 21rem;">
-        <img class="card-img-top" src="images&videos/assistant-admin-comm_thomas_31052023.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title">Assistant Administratif</h5>
-          <p class="card-text">Le tronc commun de la filière Assistant Administratif donne au stagiaire les compétences
-             nécessaires pour découvrir les métiers de gestion et du commerce,...</p>
-          <a href="https://www.myway.ac.ma/fr/filiere/GC_AA_T" class="btn btn-primary" target="_blank">More details</a>
-        </div>
-      </div>
-    </div>
-</section>
-<!-- footer -->
-  <?php 
-       
-       include("footer.html") ;
-  
-  ?>
+    </section>
+    <!-- footer -->
+    <?php        
+        include("footer.html") ;
+    ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
-         <!-- script pour menu on click -->
-         <script>
-        
-            const menuToggle = document.querySelector('.menu-toggle');
-            const navLinks = document.querySelector('.nav-links');
-    
-            menuToggle.addEventListener('click', () => {
-                navLinks.classList.toggle('active');
-            });
-        </script>
+    <!-- script pour menu on click -->
+    <script>
+        const menuToggle = document.querySelector('.menu-toggle');
+        const navLinks = document.querySelector('.nav-links');
+        menuToggle.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    </script>
 </body>
 </html>
